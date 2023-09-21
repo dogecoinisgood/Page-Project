ae9c775b8fc6ad5aec338a7dd8a8cf9efbeeba08
linche <linche0502@yahoo.com.tw>
2023-09-21 18:15:55 +0800

    c

--- garbage/labelMarker.py
+++ garbage/labelMarker.py
@@ -46,33 +46,38 @@ df= pd.DataFrame([
 
 # 找到所有放在待標記資料夾的圖片
 unMarkImages= os.listdir(base_path+"/data/images")
+# 如果有寫target_file，則直接抓取該檔案的index，並從該張圖片開始
 if target_file:
     nowImageNum= unMarkImages.index(target_file)
 else:
-    # 找到所有放在待標記資料夾的圖片中，第一個沒有標記的圖片編號
+    # 否則找到所有放在待標記資料夾的圖片中，第一個沒有標記的圖片編號
     nowImageNum= 0
     for i,image in enumerate(unMarkImages):
         if image not in df["name"].values:
             nowImageNum= i
             break
-# 讀取目標的圖片
+
+# 讀取目標的圖片 (棄用，因為後面在showRectangles()讀取框的時候，會在)
 # img = cv2.imread((base_path+'/data/images/'+unMarkImages[nowImageNum]), cv2.IMREAD_COLOR)
-img= cv2.imdecode(np.fromfile((base_path+'/data/images/'+unMarkImages[nowImageNum]), dtype=np.int8), -1)
+# img= cv2.imdecode(np.fromfile((base_path+'/data/images/'+unMarkImages[nowImageNum]), dtype=np.int8), -1)
 
 
 # 紀錄左鍵按下，開始拖曳時的點
 dragPoint= None
-# 紀錄同一張圖的其他框, rectangles= [{"WASTE_1":"xx,xx..."}, {"WASTE_2":"yy,yy..."}]
+# 紀錄同一張圖的其他框, rectangles= [{"WASTE_1":np.array([[[x1,y1],[x2,y2], ...]])}, {"WASTE_2":[[]]...}]
 rectangles= []
 def showRectangles():
     global rectangles, img
     # img = cv2.imread((base_path+'/data/images/'+unMarkImages[nowImageNum]), cv2.IMREAD_COLOR)
+    # 用imread會無法讀取有中文的路徑，所以用np讀取該圖片轉成np.array，在丟到cv2.imdecode讀取
     img= cv2.imdecode(np.fromfile((base_path+'/data/images/'+unMarkImages[nowImageNum]), dtype=np.int8), -1)
     cv2.setWindowTitle('image', unMarkImages[nowImageNum])
     for polygon in rectangles:
         label, points= list(polygon.keys())[0], list(polygon.values())[0]
-        # start, end= points
-        # cv2.rectangle(img, start, end, (0,255,0), 2)
+        # 原始資料在四邊形時的順序多為(左上,左下,右上,右下)，所以做特別處理(預定)
+        # if len(points) == 4:
+        #     cv2.rectangle(img, )
+        # else:
         cv2.polylines(img, points, 1, (0, 255, 0), 5)
         cv2.putText(img, label, points[0][0], None, 0.75,(255,255,255),3)
         cv2.putText(img, label, points[0][0], None, 0.75,(0,0,255), 2)
@@ -80,16 +85,18 @@ def showRectangles():
 
 def readRectanglesFromDf():
     global df, nowImageNum, rectangles
+    # 先嘗試搜尋dataFrame中有沒有現在正在看的圖片
     newSeries= df.loc[df['name'] == unMarkImages[nowImageNum]]
     rectangles= []
+    # 有的話就讀取
     if not newSeries.empty:
         newSeries= newSeries.iloc[0]
+        # 讀取這筆資料中的所有label和label的座標位置
         for polygon in newSeries['polygon']:
             label, points= list(polygon.keys())[0], list(polygon.values())[0]
-            # p0, _, _, p3= points.split(";")
-            # p0, p3= (int(p0.split(",")[0]), int(p0.split(",")[1])), (int(p3.split(",")[0]), int(p3.split(",")[1]))
-            # rectangles.append({label:[p0, p3]})
+            # 格式為: {"WASTE_1":np.array([[[x1,y1],[x2,y2], ...]])}
             rectangles.append({label: np.array([[[dot for dot in point.split(",")] for point in points.split(";")]], dtype=np.int32)})
+    # 讀取完之後記得顯示出所有的框框和label名
     showRectangles()
 readRectanglesFromDf()
 
@@ -105,7 +112,7 @@ def OnMouseAction(event,x,y,flags,param):
     # 左鍵放開
     elif event == cv2.EVENT_LBUTTONUP:
         # 紀錄畫下的框
-        # rectangles.append({nowLabel:[(dragPoint[0],dragPoint[1]), (x,y)]})
+        # rectangles格式為: {"WASTE_1":np.array([[[x1,y1],[x2,y2], ...]])}
         rectangles.append({nowLabel:np.array([[[dragPoint[0],dragPoint[1]], [x,dragPoint[1]], [x,y], [dragPoint[0],y]]],dtype=np.int32)})
         showRectangles()
         # 重設開始座標
@@ -126,22 +133,25 @@ def OnMouseAction(event,x,y,flags,param):
 def saveXML():
     global img, rectangles, df, nowImageNum, tree, root
     
+    # 建立一個XML的image元素。並建立一個dict以利後續在dataFrame的新增資料
     imgElement= ET.fromstring(f'<image name="{unMarkImages[nowImageNum]}" height="{img.shape[0]}" width="{img.shape[1]}"></image>')
     newDict= {"name":unMarkImages[nowImageNum],"width":img.shape[1],"height":img.shape[0], "polygon":[]}
     for polygon in rectangles:
         label, points= list(polygon.keys())[0], list(polygon.values())[0]
-        # start, end= points
-        # polygonElement= ET.fromstring(f'<polygon label="{label}" points="{start[0]},{start[1]};{start[0]},{end[1]};{end[0]},{start[1]};{end[0]},{end[1]}" />')
+        # points= np.array([[[x1,y1],[x2,y2], ...]]) => ["x1,y1", "x2,y2", ...]
         points= [f"{point[0]},{point[1]}" for point in points[0]]
         polygonElement= ET.fromstring(f'<polygon label="{label}" points="{";".join(points)}" />')
+        # 在image元素的最後面，插入一個polygon
         imgElement.insert(-1, polygonElement)
-        # newDict["polygon"].append({label: f"{start[0]},{start[1]};{start[0]},{end[1]};{end[0]},{start[1]};{end[0]},{end[1]}"})
         newDict["polygon"].append({label: f"{';'.join(points)}"})
     
+    # 先嘗試搜尋dataFrame中有沒有現在正要儲存的圖片
     nowSeries= df.loc[df['name'] == unMarkImages[nowImageNum]]
+    # 如果沒有的話，就插在dataFrame/XML裡面
     if nowSeries.empty:
         df.loc[len(df)] = newDict
         root.insert(-1, imgElement)
+    # 有的話，先找到這筆資料在df裡面的idx，再更改該筆資料
     else:
         idx= df.index[df['name'] == unMarkImages[nowImageNum]].tolist()[0]
         df['polygon'].iat[idx]= newDict['polygon']
@@ -151,6 +161,7 @@ def saveXML():
 
 def deleteImg():
     global unMarkImages, nowImageNum, df, root, tree
+    
     try:
         os.remove(base_path+ "/data/images/"+ unMarkImages[nowImageNum])
     except: pass
