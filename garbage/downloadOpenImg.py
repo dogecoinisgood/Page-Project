786d067beea2778324797cc28d2de317b368c0b2
kuo-chen-cahn <wankgu9988@gmail.com>
2023-09-22 10:21:00 +0800

    c

---
+++ garbage/downloadOpenImg.py
@@ -0,0 +1,13 @@
+import fiftyone as fo
+import fiftyone.zoo as foz
+
+dataset = foz.load_zoo_dataset(
+    "open-images-v6",
+    split="train",  # 下载训练集
+    label_types=["detections"],  # 下载目标检测标注文件
+    classes=["Lighter"],  # 下载数据集中的某几类别
+    max_samples=1500,  # 下载图片数目
+    only_matching=True,  # 只下载匹配到类别的图片
+    dataset_dir=".",  # 下载到当前目录
+)
+
