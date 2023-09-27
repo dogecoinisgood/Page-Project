1c0cbddbb73e18444606e41f73b129458750309e
linche <linche0502@yahoo.com.tw>
2023-09-27 18:16:19 +0800

    c

--- crawler/text-break.py
+++ crawler/text-break.py
@@ -18,22 +18,23 @@ data= [row[0] for row in data]
 hMarkov= {}
 lastGramFreqs= {}
 for i in range(5):
-    contentGramFreqs= None
+    # 將data中所有文章的i字Counter都集合起來
+    contentGramFreqs= collections.Counter()
     for content in data:
         gramFreqs= collections.Counter(ngrams([*content], i))
-        if contentGramFreqs:
-            contentGramFreqs+= gramFreqs
-        else:
-            contentGramFreqs= gramFreqs
-    
+        contentGramFreqs+= gramFreqs
+    # 從有了lastGramFreqs的第2個迴圈開始，都要進行比對
     if lastGramFreqs:
         for gramFreq in contentGramFreqs:
-            hMarkov[gramFreq]= contentGramFreqs[gramFreq]/ lastGramFreqs[gramFreq[:-1]]
-    
+            if contentGramFreqs[gramFreq] >= len(data)/2:
+                # ex: 所有文章中，'屈臣氏'出現的次數/'屈臣'出現的次數。這個數值越接近1，代表越有可能是一個不可分割的詞
+                hMarkov[gramFreq]= contentGramFreqs[gramFreq]/ lastGramFreqs[gramFreq[:-1]]
+    # 將這一個迴圈所做的(i字)counter，留到下一個迴圈，以供下一個迴圈(i+1字)的比對
     lastGramFreqs= contentGramFreqs
 
 hMarkov= dict(sorted(hMarkov.items(), key=lambda x:x[1], reverse=True))
 
 for i,m in enumerate(hMarkov):
-    if i < 10:
+    if i < 30:
         print(m, hMarkov[m])
+
